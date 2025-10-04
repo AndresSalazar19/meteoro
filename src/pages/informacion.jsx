dd8970332c7c6d345f7f90142efc5795a7a6abbc
Mariu Andrade Veloz <maanvelo@espol.edu.ec>
2025-10-04 10:12:03 -0500

    listameteoritos e info

--- src/pages/informacion.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Informacion() {
-  return (
-    <div className="text-center text-3xl text-white pt-20 flex justify-center items-center"> 
-        Página de Información (en construcción)
-    </div>
-  );
-}
